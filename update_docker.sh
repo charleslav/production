7ed1173c2d3c60c745b786f2926045d8c71eab91
charleslav <charles01_lv-2000@hotmail.com>
2025-03-28 06:05:26 -0400

    fix: README.md cap and update_docker.sh new name

--- update_docker.sh
+++ update_docker.sh
@@ -2,6 +2,6 @@ source .env
 
 export CR_PAT=$TOKEN_GHCR ; echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin
 
-docker pull ghcr.io/$GIT_USERNAME/$PROJECT_NAME:latest
+docker pull ghcr.io/$GIT_USERNAME/$IMAGE_NAME:latest
 
-docker run -p 80:80 $PROJECT_NAME
+docker run -p 80:80 $IMAGE_NAME
