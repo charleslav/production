df4f04c4a43ccb5df3e5f35e94bcb6824f2ee11d
charleslav <charles01_lv-2000@hotmail.com>
2025-03-28 07:21:34 -0400

    fix: start run in background

--- start_docker.sh
+++ start_docker.sh
@@ -4,4 +4,4 @@ export CR_PAT=$TOKEN_GHCR ; echo $CR_PAT | docker login ghcr.io -u USERNAME --pa
 
 docker pull ghcr.io/$GIT_USERNAME/$IMAGE_NAME:latest
 
-docker run -p 80:80 ghcr.io/$GIT_USERNAME/$IMAGE_NAME
+docker run -d -p 80:80 ghcr.io/$GIT_USERNAME/$IMAGE_NAME
