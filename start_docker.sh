062a1bf53cc3a7f78362253a5119d0b447163a09
charleslav <charles01_lv-2000@hotmail.com>
2025-03-28 07:04:49 -0400

    fix: reboot after start, better name for comprehension

--- start_docker.sh
+++ start_docker.sh
@@ -1,15 +1,7 @@
-# Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+source .env
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+export CR_PAT=$TOKEN_GHCR ; echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo usermod -aG docker $USER
+docker pull ghcr.io/$GIT_USERNAME/$IMAGE_NAME:latest
+
+docker run -p 80:80 ghcr.io/$GIT_USERNAME/$IMAGE_NAME
--- update_docker.sh
+++
@@ -1,7 +0,0 @@
-source .env
-
-export CR_PAT=$TOKEN_GHCR ; echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin
-
-docker pull ghcr.io/$GIT_USERNAME/$IMAGE_NAME:latest
-
-docker run -p 80:80 ghcr.io/$GIT_USERNAME/$IMAGE_NAME
