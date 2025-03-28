daed756aa92bb6623b9db44e55a972c74a70c57f
charleslav <charles01_lv-2000@hotmail.com>
2025-03-28 07:59:00 -0400

    feat: add webhook

---
+++ webhooks.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+basic_url = "/webhook"
+
+@app.route(basic_url + "/update", methods=['POST'])
+def webhook():
+    os.system("bash start_docker.sh")
+    return "Webhook received!"
+
+app.run(host='0.0.0.0', port=8000)
